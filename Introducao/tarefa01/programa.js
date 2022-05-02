61aacdce6d450d543e23eda0e0fcbd85002f3c37
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-05-02 11:14:20 -0300

    Tarefa 01

---
+++ Introducao/tarefa01/programa.js
@@ -0,0 +1,9 @@
+/* Crie um arquivo com nome 'programa' e crie duas variáveis e imprima a soma delas*/
+
+const soma = (a,b) => a+b;
+var numero1 = 15;
+var numero2 = 55;
+
+console.log("A soma dos dois números é " + soma(numero1,numero2));
+
+
