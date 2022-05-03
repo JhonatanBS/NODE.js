5d20f5525504a42e94bc48b15ea1a37e555567bd
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-05-03 09:48:41 -0300

    Módulos internos

---
+++ Fundamentos/modulo_interno/index.js
@@ -0,0 +1,8 @@
+const meuModulo = require("./meu_modulo");
+var soma = meuModulo.soma;
+var subtracao = meuModulo.subtracao;
+
+soma(10,30);
+soma(30,50);
+subtracao(100,15);
+subtracao(50,25);
---
+++ Fundamentos/modulo_interno/meu_modulo.js
@@ -0,0 +1,5 @@
+module.exports = {
+    soma(a,b){ console.log(a + b)}   
+}
+
+module.exports.subtracao = function sub(a,b){ console.log(a - b)}
