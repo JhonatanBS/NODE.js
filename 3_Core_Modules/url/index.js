39a0683e6de3c5d5b374264147ea4080f6c68acd
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-05-05 08:58:21 -0300

    Módulo url

---
+++ 3_Core_Modules/url/index.js
@@ -0,0 +1,10 @@
+const url = require("url");
+const address = "https://www.meusite.com.br/catalog?produtos=cadeira";
+const parsedURL = new url.URL(address);
+
+console.log(parsedURL.host);
+console.log(parsedURL.pathname);
+console.log(parsedURL.search);
+console.log(parsedURL.searchParams);
+console.log(parsedURL.searchParams.get("produtos"));
+console.log(parsedURL.href);
