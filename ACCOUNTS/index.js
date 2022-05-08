3e9cc5d56b3cca661ceb892477b0f9dbc1d5dd1b
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-05-08 19:55:18 -0300

    Projeto ACCOUNTS Setup

---
+++ ACCOUNTS/index.js
@@ -0,0 +1,8 @@
+// Módulos Externos
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+
+// Módulos internos
+const fs = require("fs");
+
+console.log("Projeto iniciado com sucesso!");
