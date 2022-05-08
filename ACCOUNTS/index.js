c1b6109deb74e5f2a4cc5e97ea61d4b785cee91d
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-05-08 20:05:55 -0300

    Inicialização do projeto

--- ACCOUNTS/index.js
+++ ACCOUNTS/index.js
@@ -5,4 +5,20 @@ const chalk = require("chalk");
 // Módulos internos
 const fs = require("fs");
 
-console.log("Projeto iniciado com sucesso!");
+operation();
+
+function operation(){
+    inquirer.prompt([
+        {
+        type: "list",
+        name: "action",
+        message: "O que você deseja fazer?",
+        choices: ["Criar Conta", "Consultar Saldo", "Depositar", "Sacar", "Sair"],
+        }
+    ]).then((answer)=>{
+        const action = answer["action"];
+
+        console.log(action);
+    })
+      .catch((err)=>console.log(err));
+}
