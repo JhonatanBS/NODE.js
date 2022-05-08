120b9b8cb266e81b9bcf64199f4064ce4aeaf90b
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-05-08 20:45:39 -0300

    Finalizando a opção de criar conta

--- ACCOUNTS/index.js
+++ ACCOUNTS/index.js
@@ -28,5 +28,38 @@ function operation(){
 // Create an ACCOUNT
 function createAccount(){
     console.log(chalk.bgGreen.black("Parabéns por escolher o nosso Banco!"));
-    console.log(chalk.green("Defina as opções da sua conta a seguir"));
+    console.log(chalk.green("Defina as opções da sua conta a seguir:"));
+
+    buildAccount();
+}
+
+function buildAccount(){
+    inquirer.prompt([
+        {
+            name: "accountName",
+            message: "Digite um nome para sua Conta:",
+        },
+    ]).then((answer)=>{
+        const accountName = answer["accountName"];
+
+        console.info(accountName);
+
+        if(!fs.existsSync("accounts")){
+            fs.mkdirSync("accounts");
+        }
+
+        if(fs.existsSync(`accounts/${accountName}.json`)){
+            console.log(chalk.bgRed.black("Está conta já existe, escolha outro nome!"));
+            buildAccount();
+            return;
+        }
+
+        fs.writeFileSync(`accounts/${accountName}.json`, `{"balance": 0}`, function(err){
+            console.log(err);
+        });
+
+        console.log(chalk.green("Parabéns, a sua conta foi criada!"));
+        operation();
+    })
+      .catch((err)=> console.log(err));
 }
