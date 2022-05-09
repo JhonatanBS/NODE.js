9cfefe6de51431ee6d1afcb251f932632b8000d7
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-05-09 11:05:48 -0300

    sendFile

---
+++ 4_express/renderFile/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
+
+const basePath = path.join(__dirname,"templates");
+
+app.get("/", (req, res)=>{
+    res.sendFile(`${basePath}/index.html`);
+});
+
+app.listen(port, ()=>{
+    console.log(`O servidor está rodando na porta: ${port}`);
+});
