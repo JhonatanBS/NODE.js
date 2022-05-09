5e45c70ffc3bc733daf24bd80fb2598c554dfdb9
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-05-09 08:23:42 -0300

    Opção de saque

--- ACCOUNTS/index.js
+++ ACCOUNTS/index.js
@@ -25,7 +25,7 @@ function operation(){
         }else if(action === "Depositar"){
             deposit();
         }else if(action === "Sacar"){
-
+            withdraw();
         }else if(action === "Sair"){
             console.log(chalk.bgBlue.black("Obrigado por usar o ACCOUNTS"));
             process.exit();
@@ -170,3 +170,64 @@ function getAccountBalance() {
       })
   }
   
+  // get money from account
+function withdraw() {
+    inquirer
+      .prompt([
+        {
+          name: 'accountName',
+          message: 'Qual o nome da sua conta?',
+        },
+      ])
+      .then((answer) => {
+        const accountName = answer['accountName']
+  
+        if (!checkAccount(accountName)) {
+          return withdraw()
+        }
+  
+        inquirer
+          .prompt([
+            {
+              name: 'amount',
+              message: 'Quanto você deseja sacar?',
+            },
+          ])
+          .then((answer) => {
+            const amount = answer['amount']
+  
+            removeAmount(accountName, amount)
+            operation()
+          })
+      })
+  }
+  
+  function removeAmount(accountName, amount) {
+    const accountData = getAccount(accountName)
+  
+    if (!amount) {
+      console.log(
+        chalk.bgRed.black('Ocorreu um erro, tente novamente mais tarde!'),
+      )
+      return withdraw()
+    }
+  
+    if (accountData.balance < amount) {
+      console.log(chalk.bgRed.black('Valor indisponível!'))
+      return withdraw()
+    }
+  
+    accountData.balance = parseFloat(accountData.balance) - parseFloat(amount)
+  
+    fs.writeFileSync(
+      `accounts/${accountName}.json`,
+      JSON.stringify(accountData),
+      function (err) {
+        console.log(err)
+      },
+    )
+  
+    console.log(
+      chalk.green(`Foi realizado um saque de R$${amount} da sua conta!`),
+    )
+  }
