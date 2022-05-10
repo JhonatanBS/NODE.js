875051d2badda70420f7d45450214aa33ab86149
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-05-10 07:38:12 -0300

    Método POST

---
+++ 4_express/post_dados/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
+
+const basePath = path.join(__dirname,"templates");
+
+// Ler o body
+app.use(
+    express.urlencoded({
+        extended:true,
+    })
+);
+
+app.use(express.json());
+
+app.get("/users/add", (req,res) =>{
+    res.sendFile(`${basePath}/users.html`);
+});
+
+app.post("/users/save",(req,res)=>{
+    console.log(req.body);
+
+    const name = req.body.name;
+    const age = req.body.age;
+
+    console.log(`O nome do usuário é ${name} e ele tem ${age} anos`);
+
+    res.sendFile(`${basePath}/users.html`);
+})
+
+app.get("/users/:id", (req, res)=>{
+    const id = req.params.id;
+
+    console.log(`Estamos buscando pelo usuário: ${id}`);
+
+    res.sendFile(`${basePath}/users.html`);
+});
+
+app.get("/", (req, res)=>{
+    res.sendFile(`${basePath}/index.html`);
+});
+
+app.listen(port, ()=>{
+    console.log(`O servidor está rodando na porta: ${port}`);
+});
