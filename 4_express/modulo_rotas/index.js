30a7aa1b6367525397a5900962d8fd9eafffbeb1
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-05-10 07:55:08 -0300

    Módulo para rotas 'Router'

---
+++ 4_express/modulo_rotas/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
+
+const users = require("./users");
+
+// Ler o body
+app.use(
+    express.urlencoded({
+        extended:true,
+    })
+);
+
+app.use(express.json());
+
+app.use("/users", users);
+
+app.get("/", (req, res)=>{
+    res.sendFile(`${basePath}/index.html`);
+});
+
+app.listen(port, ()=>{
+    console.log(`O servidor está rodando na porta: ${port}`);
+});
