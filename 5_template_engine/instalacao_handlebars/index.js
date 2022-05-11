4671de064dc9f9aa896397cfb3dbfb5ad5f7367c
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-05-11 08:50:24 -0300

    Instalação e configurações do handlebars

---
+++ 5_template_engine/instalacao_handlebars/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
+const port = 3000;
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.get("/", (req,res)=>{
+   res.render("home", {layout: false})
+});
+
+app.listen(port, ()=>{
+    console.log(`O servidor está funcionando na porta ${port}`);
+})
