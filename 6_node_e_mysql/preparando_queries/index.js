b229dd9a7efa3d25829585ca703ecf0b3ee79de8
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-05-14 22:03:02 -0300

    Preparando queries com ?? para colunas e ? para dados, para se proteger do sql injection

---
+++ 6_node_e_mysql/preparando_queries/index.js
@@ -0,0 +1,136 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const pool = require("./db/conn");
+
+const app = express();
+
+app.use(express.urlencoded({
+    extended:true
+}));
+
+app.use(express.json());
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
+
+app.post("/books/insertbook", (req,res)=>{
+    const title = req.body.title;
+    const pages = req.body.pages;
+
+    console.log(req.body);
+
+    const sql = `INSERT INTO books(??,??) VALUES(?,?)`;
+    const data = ["title", "pages", title, pages];
+
+    pool.query(sql,data,function(err){
+        if(err){
+            console.log(err);
+            return;
+        }
+
+        res.redirect("/");
+    });
+});
+
+app.get("/books",(req,res)=>{
+    const sql = "SELECT * FROM books";
+  
+    pool.query(sql, function(err, data){
+      if(err){
+          console.log(err);
+          return;
+      }
+  
+      const books = data;
+      console.log(books);
+
+      res.render("books",{books});
+    });
+  });
+
+app.get("/", (req,res)=>{
+    res.render("home");
+});
+
+app.use("/book/:id", (req,res)=>{
+
+    const id = req.params.id;
+
+    const sql = `SELECT * FROM books WHERE ??=?`;
+
+    const data = ["id",id];
+
+    pool.query(sql,data,function(err, data){
+        if(err){
+            console.log(err);
+            return;
+        }
+
+        const book = data[0];
+
+        res.render("book", {book});
+    });
+});
+
+app.get("/books/edit/:id",(req,res)=>{
+   const id = req.params.id;
+
+   const sql = `SELECT * FROM books WHERE ??=?`;
+
+   const data = ["id",id];
+
+   pool.query(sql,data,function(err,data){
+    if(err){
+        console.log(err);
+        return;
+    }
+
+    const editbook = data[0];
+
+    res.render("editbook", {editbook})
+   });
+});
+
+app.post("/books/updatebook",(req,res)=>{
+    const id = req.body.id;
+    const title = req.body.title;
+    const pages = req.body.pages;
+
+    const sql = `UPDATE books SET ??=?, ??=? WHERE ??=?`;
+
+    const data = ["title", title, "pages", pages,"id", id];
+
+    pool.query(sql,data,function(err){
+        if(err){
+            console.log(err);
+            return;
+        }
+
+        res.redirect("/books");
+    });
+});
+
+app.post("/books/remove/:id", (req,res)=>{
+   const id = req.params.id;
+
+   const sql = `DELETE FROM books WHERE ??=?`;
+
+   const data = ["id", id];
+
+   pool.query(sql,data,function(err){
+    if(err){
+        console.log(err);
+        return;
+    }
+
+    res.redirect("/books");
+
+   });
+});
+
+
+app.listen(3000,()=>{
+    console.log("Servidor funcionando na porta 3000");
+ });
