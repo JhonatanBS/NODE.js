bc5c07c2bd61c07808befc5c3c04b2be2bbee34b
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-05-22 15:35:04 -0300

    Iniciando a autenticação

--- TOUGHTS/index.js
+++ TOUGHTS/index.js
@@ -14,6 +14,7 @@ const User = require("./models/User");
 
 // Import Routes
 const toughtsRoutes = require("./routes/toughtsRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 // Import Controller
 const ToughtController = require("./controllers/ToughtController");
@@ -54,6 +55,7 @@ app.use(
 
 // Routes
 app.use("/toughts", toughtsRoutes);
+app.use("/", authRoutes);
 
 app.get("/", ToughtController.showToughts);
 
