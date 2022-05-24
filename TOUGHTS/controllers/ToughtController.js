02654bce87286d6424ae8d9f6d0b889d98b61a31
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-05-24 14:51:56 -0300

    Resgatando Pensamentos para o dashboard

--- TOUGHTS/controllers/ToughtController.js
+++ TOUGHTS/controllers/ToughtController.js
@@ -8,7 +8,23 @@ module. exports = class ToughtController{
     }
 
     static async dashboard(req,res){
-        res.render("toughts/dashboard");
+        const userId = req.session.userid;
+
+        const user = await User.findOne({
+            where:{
+                id: userId
+            },
+            include: Tought
+        })
+    
+        // check if user exists
+        if(!user){
+            res.redirect("/login");
+        }
+
+        const toughts = user.Toughts.map((result)=> result.dataValues);
+
+        res.render("toughts/dashboard", {toughts});
     }
 
     static createTought(req,res){
