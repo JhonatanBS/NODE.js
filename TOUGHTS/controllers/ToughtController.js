b25a6c26b2a7138dfcfeab17b3070e37f9913cdc
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-05-24 14:36:25 -0300

    Criando formulário de criação de pensamento e inserindo no banco de dados

--- TOUGHTS/controllers/ToughtController.js
+++ TOUGHTS/controllers/ToughtController.js
@@ -10,4 +10,27 @@ module. exports = class ToughtController{
     static async dashboard(req,res){
         res.render("toughts/dashboard");
     }
+
+    static createTought(req,res){
+        res.render("toughts/create");
+    }
+
+    static async createToughtSave(req,res){
+        const tought = {
+            title: req.body.title,
+            UserId: req.session.userid,
+        }
+
+        try {
+            await Tought.create(tought);
+
+            req.flash("message","Pensamento criado com sucesso!");
+
+            req.session.save(()=>{
+                res.redirect("/toughts/dashboard")
+            })
+        } catch (error) {
+            console.log(`Aconteceu um erro:` + error);
+        }
+    }
 }
--- TOUGHTS/routes/toughtsRoutes.js
+++ TOUGHTS/routes/toughtsRoutes.js
@@ -7,6 +7,8 @@ const ToughtController = require("../controllers/ToughtController");
 // Helpers
 const checkAuth = require("../helpers/auth").checkAuth;
 
+router.get("/add",checkAuth, ToughtController.createTought);
+router.post("/add",checkAuth, ToughtController.createToughtSave);
 router.get("/dashboard",checkAuth, ToughtController.dashboard);
 router.get("/", ToughtController.showToughts);
 
