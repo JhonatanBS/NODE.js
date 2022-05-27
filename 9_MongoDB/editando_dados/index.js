3a02464b1c772bb48d0d046349751655e18740af
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-05-27 12:29:38 -0300

    Atualizando dados com mongoDB

---
+++ 9_MongoDB/editando_dados/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+const conn = require("./db/conn");
+
+//Routes
+const productsRoutes = require("./routes/productsRoutes");
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use("/products", productsRoutes);
+
+app.listen(3000);
