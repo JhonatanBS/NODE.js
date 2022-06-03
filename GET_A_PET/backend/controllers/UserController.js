af27f482a064cbb6460d626f0efe782c8230abff
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-06-03 14:51:32 -0300

    Resgatando usuário por id

--- GET_A_PET/backend/controllers/UserController.js
+++ GET_A_PET/backend/controllers/UserController.js
@@ -122,4 +122,17 @@ module.exports = class UserController {
 
           res.status(200).send(currentUser);
       }
+
+      static async getUserById(req,res){
+          const { id } = req.params;
+
+          const user = await User.findById(id).select("-password");
+
+          if(!user){
+             res.status(422).json({ message: "Usuário não encontrado!" });
+             return;
+          }
+
+          res.status(200).json({user});
+      }
 }
