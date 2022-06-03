bab08b20035c740631f5364d3cb9db2b399f2b71
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-06-03 15:14:33 -0300

    Verificando token de usuário

--- GET_A_PET/backend/controllers/UserController.js
+++ GET_A_PET/backend/controllers/UserController.js
@@ -135,4 +135,9 @@ module.exports = class UserController {
 
           res.status(200).json({user});
       }
+
+      static async editUser(req,res){
+        res.status(200).json({ message: "Deu certo update!" });
+        return;
+      }
 }
