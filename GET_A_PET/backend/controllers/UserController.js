eb508c9d51d2e92b7bad2c7befbd253ea9e88825
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-06-15 09:03:30 -0300

    Fazendo upload de imagem do usuário

--- GET_A_PET/backend/controllers/UserController.js
+++ GET_A_PET/backend/controllers/UserController.js
@@ -146,7 +146,9 @@ module.exports = class UserController {
 
         const { name, email, phone, password, confirmpassword} = req.body;
 
-        let image = "";
+        if(req.file){
+           user.image = req.file.filename;
+        }
 
         // Validations
         if(!name){
