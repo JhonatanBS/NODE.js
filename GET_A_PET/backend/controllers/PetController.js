976609d44e54f1e0e843f4cebda918135c7063fd
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-06-22 10:49:58 -0300

    Função para resgatar todos os pets do usuário

--- GET_A_PET/backend/controllers/PetController.js
+++ GET_A_PET/backend/controllers/PetController.js
@@ -80,4 +80,16 @@ module.exports = class PetController{
 
         res.status(200).json({pets:pets})
     }
+
+    static async getAllUserPets(req,res){
+        //get user from token
+        const token = getToken(req);
+        const user = await getUserByToken(token);
+
+        const pets = await Pet.find({ "user._id": user._id}).sort("-createdAt");
+
+        res.status(200).json({
+            pets,
+        })
+    }
 }
