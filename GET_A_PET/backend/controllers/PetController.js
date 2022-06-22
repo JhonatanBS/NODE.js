cacc55784b7077a05b43c5678f5a0b860efb876d
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-06-22 14:08:45 -0300

    Resgatando os pets pelo id

--- GET_A_PET/backend/controllers/PetController.js
+++ GET_A_PET/backend/controllers/PetController.js
@@ -3,6 +3,7 @@ const Pet = require("../models/Pet");
 // helpers
 const getToken = require("../helpers/get-token");
 const getUserByToken = require("../helpers/get-user-by-token");
+const ObjectId = require("mongoose").Types.ObjectId;
 
 module.exports = class PetController{
     // create a pet
@@ -103,5 +104,27 @@ module.exports = class PetController{
          res.status(200).json({
              pets,
          });
+    }
+
+    static async getPetById(req,res){
+        const { id } = req.params;
+
+        if(!ObjectId.isValid(id)){
+            res.status(422).json({
+                message: "ID inválido"
+            });
+            return;
+        }
+
+        // check if pet exists
+        const pet = await Pet.findOne({_id:id});
+
+        if(!pet){
+            res.status(404).json({ message: "Pet não encontrado!"});
+        }
+
+        res.status(200).json({
+            pet: pet,
+        });
     }
 }
