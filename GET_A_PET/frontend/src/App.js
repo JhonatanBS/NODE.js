d1385b42c230749bf5bd17e9d0c298382673cdb1
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-06-23 14:39:01 -0300

    Inserindo o React Router

--- GET_A_PET/frontend/src/App.js
+++ GET_A_PET/frontend/src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+/* Pages */
+import Login from "./components/pages/Auth/Login";
+import Register from "./components/pages/Auth/Register";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Get A Pet</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+          
+       
+
+        <Route path="/register" element={<Register/>}/>
+          
+      
+
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </Router>
   );
 }
 
