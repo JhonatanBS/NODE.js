5b24baf6863c6698e61ad9b13e283a3b88a77415
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-06-27 11:28:28 -0300

    CSS do Navbar e Footer

--- GET_A_PET/frontend/src/components/layout/Navbar.js
+++ GET_A_PET/frontend/src/components/layout/Navbar.js
@@ -2,10 +2,12 @@ import { Link } from "react-router-dom";
 
 import Logo from "../../assets/img/logo.png"
 
+import styles from "./Navbar.module.css"
+
 const Navbar = () => {
     return (
-      <nav>
-        <div>
+      <nav className={styles.navbar}>
+        <div className={styles.navbar_logo}>
            <img src={Logo} alt="Get A Pet"/>
            <h2>Get A Pet</h2>
         </div>
