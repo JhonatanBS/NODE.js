626874678afe489f3ee370b26ffa436c4da3ecf2
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-06-27 10:50:21 -0300

    Estruturando o Navbar e o Footer

--- GET_A_PET/frontend/src/App.js
+++ GET_A_PET/frontend/src/App.js
@@ -5,9 +5,14 @@ import Login from "./components/pages/Auth/Login";
 import Register from "./components/pages/Auth/Register";
 import Home from "./components/pages/Home";
 
+/* Components */
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+
 function App() {
   return (
     <Router>
+      <Navbar/>
       <Routes>
         <Route path="/login" element={<Login/>}/>
           
@@ -19,6 +24,7 @@ function App() {
 
         <Route path="/" element={<Home/>}/>
       </Routes>
+      <Footer/>
     </Router>
   );
 }
---
+++ GET_A_PET/frontend/src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        <span className="bold">Get A Pet</span> &copy; 2022
+      </p>
+    </footer>
+  )
+}
+
+export default Footer;
