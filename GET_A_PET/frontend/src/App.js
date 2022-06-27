169cd7f7b009391cf8f1864926973c8eee1e798a
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-06-27 12:55:19 -0300

    Criando componente Container

--- GET_A_PET/frontend/src/App.js
+++ GET_A_PET/frontend/src/App.js
@@ -8,22 +8,21 @@ import Home from "./components/pages/Home";
 /* Components */
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import Container from "./components/layout/Container";
 
 function App() {
   return (
     <Router>
       <Navbar/>
+      <Container>
       <Routes>
         <Route path="/login" element={<Login/>}/>
-          
-       
 
         <Route path="/register" element={<Register/>}/>
-          
-      
-
+        
         <Route path="/" element={<Home/>}/>
       </Routes>
+      </Container>
       <Footer/>
     </Router>
   );
