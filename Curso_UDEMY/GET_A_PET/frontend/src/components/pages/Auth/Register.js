0b06ffb919149e752b375cee77e693be751eeb8b
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-07-10 09:38:36 -0300

    CSS e criação de componentes para o formulário

--- Curso_UDEMY/GET_A_PET/frontend/src/components/pages/Auth/Register.js
+++ Curso_UDEMY/GET_A_PET/frontend/src/components/pages/Auth/Register.js
@@ -1,7 +1,64 @@
+import Input from "../../form/Input"
+
+import { Link } from "react-router-dom"
+
+import styles from "../../form/Form.module.css"
+
 function Register(){
+
+    function handleChange(e){
+
+    }
+
     return(
-     <section>
+     <section className={styles.form_container}>
          <h1>Register</h1>
+         <form>
+            <Input
+              text="Nome"
+              type="text"
+              name="name"
+              placeholder="Digite seu nome"
+              handleOnChange={handleChange}
+            />
+
+            <Input
+              text="Telefone"
+              type="text"
+              name="phone"
+              placeholder="Digite o seu telefone"
+              handleOnChange={handleChange}
+            />
+
+            <Input
+              text="E-mail"
+              type="email"
+              name="email"
+              placeholder="Digite seu e-mail"
+              handleOnChange={handleChange}
+            />
+
+            <Input
+              text="Senha"
+              type="password"
+              name="password"
+              placeholder="Digite a sua senha"
+              handleOnChange={handleChange}
+            />
+
+            <Input
+              text="Confirmação de senha"
+              type="password"
+              name="confirmpassword"
+              placeholder="Confirme a sua senha"
+              handleOnChange={handleChange}
+            />
+
+            <input type="submit" value="Cadastrar"/>
+         </form>
+         <p>
+            Já tem conta? <Link to="/login">Clique aqui.</Link>
+         </p>
      </section>
     )
  }
