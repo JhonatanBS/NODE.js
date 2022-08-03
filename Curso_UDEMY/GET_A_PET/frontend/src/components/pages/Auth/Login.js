e76e3934dbc41b713b24534e8098190abf30dfcf
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-08-03 08:31:50 -0300

    Criando formulário de login

--- Curso_UDEMY/GET_A_PET/frontend/src/components/pages/Auth/Login.js
+++ Curso_UDEMY/GET_A_PET/frontend/src/components/pages/Auth/Login.js
@@ -1,7 +1,43 @@
+import { useState, useContext } from "react";
+import Input from "../../form/Input";
+
+import styles from "../../form/Form.module.css";
+
+import { Context } from "../../../context/UserContext";
+import { Link } from "react-router-dom";
+
 function Login(){
+
+    function handleChange(e) {
+    
+    }
+
     return(
-     <section>
+     <section className={styles.form_container}>
          <h1>Login</h1>
+
+         <form>
+            <Input 
+              text="E-mail"
+              type="email"
+              name="email"
+              placeholder="Digite o seu e-mail"
+              handleOnChange={handleChange}
+            />
+
+            <Input 
+              text="Senha"
+              type="password"
+              name="password"
+              placeholder="Digite a sua senha"
+              handleOnChange={handleChange}
+            />
+
+            <input type="submit" value="Entrar" />
+         </form>
+         <p>
+            Não tem conta? <Link to="/register">Clique aqui</Link>
+         </p>
      </section>
     )
  }
