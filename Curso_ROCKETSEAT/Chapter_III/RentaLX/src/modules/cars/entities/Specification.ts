d64e03cdf7b35719ee7c1c8b5aa129ef99bb90db
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-09-17 14:34:59 -0300

    Creating migrations from specifications

--- Curso_ROCKETSEAT/Chapter_III/RentaLX/src/modules/cars/entities/Specification.ts
+++ Curso_ROCKETSEAT/Chapter_III/RentaLX/src/modules/cars/entities/Specification.ts
@@ -1,10 +1,19 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
+@Entity("specifications")
 class Specification {
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
-  create_at: Date;
+
+  @Column()
+  created_at: Date;
 
   constructor() {
     if (!this.id) {
