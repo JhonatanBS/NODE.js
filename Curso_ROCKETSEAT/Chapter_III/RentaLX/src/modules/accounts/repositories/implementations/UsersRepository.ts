e02966ebbe11b77b9aaff57c9b9bff6bc6eef1f7
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-09-19 15:48:20 -0300

    Creating password with bcryptjs

--- Curso_ROCKETSEAT/Chapter_III/RentaLX/src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ Curso_ROCKETSEAT/Chapter_III/RentaLX/src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -26,4 +26,9 @@ export class UsersRepository implements IUsersRepository {
 
     await this.repository.save(user);
   }
+
+  async findByEmail(email: string): Promise<User> {
+    const user = this.repository.findOne({ email });
+    return user;
+  }
 }
