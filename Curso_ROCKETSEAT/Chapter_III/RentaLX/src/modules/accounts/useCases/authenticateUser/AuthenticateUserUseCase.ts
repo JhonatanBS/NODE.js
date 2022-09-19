d1308cbcb460ebd656ceee683683767ef2ed4219
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-09-19 18:35:58 -0300

    Creating token user

---
+++ Curso_ROCKETSEAT/Chapter_III/RentaLX/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,55 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+export class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private userRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.userRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Email or password incorrect!");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email or password incorrect!");
+    }
+
+    const token = sign({}, "284fda2c-11f6-4853-b07c-f098d0f9b7ed", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+
+    return tokenReturn;
+  }
+}
--- Curso_ROCKETSEAT/Chapter_III/RentaLX/src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ Curso_ROCKETSEAT/Chapter_III/RentaLX/src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,5 +1,5 @@
-import { inject, injectable } from "tsyringe";
 import { hash } from "bcryptjs";
+import { inject, injectable } from "tsyringe";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
