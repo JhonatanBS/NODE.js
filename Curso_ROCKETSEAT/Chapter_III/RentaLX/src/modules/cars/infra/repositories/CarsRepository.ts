2fbcdc575c504b0b30ce754dc74a951667078e43
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-10-17 11:09:52 -0300

    Continue implementation CreateCarSpecificationUseCase

--- Curso_ROCKETSEAT/Chapter_III/RentaLX/src/modules/cars/infra/repositories/CarsRepository.ts
+++ Curso_ROCKETSEAT/Chapter_III/RentaLX/src/modules/cars/infra/repositories/CarsRepository.ts
@@ -11,6 +11,7 @@ export class CarsRepository implements ICarsRepository {
   constructor() {
     this.repository = getRepository(Car);
   }
+
   async create({
     name,
     brand,
@@ -19,6 +20,7 @@ export class CarsRepository implements ICarsRepository {
     description,
     fine_amount,
     license_plate,
+    specifications,
   }: ICreateCarDTO): Promise<Car> {
     const car = this.repository.create({
       name,
@@ -28,6 +30,7 @@ export class CarsRepository implements ICarsRepository {
       description,
       fine_amount,
       license_plate,
+      specifications,
     });
 
     await this.repository.save(car);
@@ -63,5 +66,10 @@ export class CarsRepository implements ICarsRepository {
     const cars = await carsQuery.getMany();
 
     return cars;
+  }
+
+  async findById(id: string): Promise<Car> {
+    const car = await this.repository.findOne(id);
+    return car;
   }
 }
