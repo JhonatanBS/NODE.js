5c752a27379ec513d05143657bc8722780a433f5
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-10-17 09:17:43 -0300

    Create useCase of CarSpecification with tests

---
+++ Curso_ROCKETSEAT/Chapter_III/RentaLX/src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
@@ -0,0 +1,45 @@
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
+import { CreateCarSpecificationUseCase } from "./CreateCarSpecificationUseCase";
+
+let createCarSpecificationUseCase: CreateCarSpecificationUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+
+describe("Create Car Specification", () => {
+  beforeEach(() => {
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
+    createCarSpecificationUseCase = new CreateCarSpecificationUseCase(
+      carsRepositoryInMemory
+    );
+  });
+
+  it("should not be able to add a new specification to a now-existent car", async () => {
+    expect(async () => {
+      const car_id = "1234";
+      const specifications_id = ["54321"];
+
+      await createCarSpecificationUseCase.execute({
+        car_id,
+        specifications_id,
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should be able to add a new specification to the car", async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: "Car2",
+      description: "Car description",
+      daily_rate: 80.0,
+      license_plate: "ABC-4714",
+      fine_amount: 40.0,
+      brand: "Car_Brand_test",
+      category_id: "category_id",
+    });
+    const specifications_id = ["54321"];
+
+    await createCarSpecificationUseCase.execute({
+      car_id: car.id,
+      specifications_id,
+    });
+  });
+});
