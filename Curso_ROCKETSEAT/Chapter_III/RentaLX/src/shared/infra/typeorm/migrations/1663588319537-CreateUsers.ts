9d64f3fff75df66118c62d907af74fb5d58cb507
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-10-25 09:08:01 -0300

    Create migration rent

--- Curso_ROCKETSEAT/Chapter_III/RentaLX/src/shared/infra/typeorm/migrations/1663588319537-CreateUsers.ts
+++ Curso_ROCKETSEAT/Chapter_III/RentaLX/src/shared/infra/typeorm/migrations/1663588319537-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1663588319537 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
