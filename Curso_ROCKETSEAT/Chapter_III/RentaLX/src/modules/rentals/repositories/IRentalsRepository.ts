7065cac1fa9c9192717b2a783f44d386d4863ff8
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-10-25 11:04:32 -0300

    Continue register of rental

--- Curso_ROCKETSEAT/Chapter_III/RentaLX/src/modules/rentals/repositories/IRentalsRepository.ts
+++ Curso_ROCKETSEAT/Chapter_III/RentaLX/src/modules/rentals/repositories/IRentalsRepository.ts
@@ -1,6 +1,9 @@
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 
+import { ICreateRentalDTO } from "../dto/ICreateRentalDTO";
+
 export interface IRentalsRepository {
   findOpenRentalByCar(car_id: string): Promise<Rental>;
   findOpenRentalByUser(user_id: string): Promise<Rental>;
+  create(data: ICreateRentalDTO): Promise<Rental>;
 }
--- Curso_ROCKETSEAT/Chapter_III/RentaLX/src/modules/rentals/useCases/CreateRentalUseCase.ts
+++ Curso_ROCKETSEAT/Chapter_III/RentaLX/src/modules/rentals/useCases/CreateRentalUseCase.ts
@@ -1,5 +1,6 @@
 import { AppError } from "@shared/errors/AppError";
 
+import { Rental } from "../infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "../repositories/IRentalsRepository";
 
 interface IRequest {
@@ -14,7 +15,7 @@ export class CreateRentalUseCase {
     car_id,
     user_id,
     expected_return_date,
-  }: IRequest): Promise<void> {
+  }: IRequest): Promise<Rental> {
     const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
       car_id
     );
@@ -30,5 +31,13 @@ export class CreateRentalUseCase {
     if (rentalOpenToUser) {
       throw new AppError("There's a rental in progress for user!");
     }
+
+    const rental = await this.rentalsRepository.create({
+      user_id,
+      car_id,
+      expected_return_date,
+    });
+
+    return rental;
   }
 }
