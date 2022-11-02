c708bb3a471e74a254eb76bc489be809cba21c9e
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-11-02 09:40:17 -0300

    Correction of available car

--- Curso_ROCKETSEAT/Chapter_III/RentaLX/src/modules/rentals/useCases/CreateRentalUseCase.ts
+++ Curso_ROCKETSEAT/Chapter_III/RentaLX/src/modules/rentals/useCases/CreateRentalUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
 
@@ -18,7 +19,9 @@ export class CreateRentalUseCase {
     @inject("RentalsRepository")
     private rentalsRepository: IRentalsRepository,
     @inject("DayjsDateProvider")
-    private dateProvider: IDateProvider
+    private dateProvider: IDateProvider,
+    @inject("CarsRepository")
+    private carsRepository: ICarsRepository
   ) {}
   async execute({
     car_id,
@@ -59,6 +62,8 @@ export class CreateRentalUseCase {
       car_id,
       expected_return_date,
     });
+
+    await this.carsRepository.updateAvailable(car_id, false);
 
     return rental;
   }
