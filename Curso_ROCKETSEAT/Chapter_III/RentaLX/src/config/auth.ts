522b45e70017eb7867063f408028ac2e7e214a6f
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-11-12 23:04:40 -0300

    Refactor authentication of user

---
+++ Curso_ROCKETSEAT/Chapter_III/RentaLX/src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "284fda2c-11f6-4853-b07c-f098d0f9b7ed",
+  expires_in_token: "15m",
+  secret_refresh_token: "5b6cfe40-6a18-47d0-9e22-1335c41d224f",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
