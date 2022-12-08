aae67ae6ef7ce3eb87bb2bda771e43964a2f62a5
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-12-08 14:54:55 -0300

    Create Provider of upload

--- Curso_ROCKETSEAT/Chapter_III/RentaLX/src/shared/infra/http/routes/cars.routes.ts
+++ Curso_ROCKETSEAT/Chapter_III/RentaLX/src/shared/infra/http/routes/cars.routes.ts
@@ -17,7 +17,7 @@ const listAvailableCarsController = new ListCarsAvailableController();
 const createCarSpecificationController = new CreateCarSpecificationController();
 const uploadCarImagesController = new UploadCarImagesController();
 
-const upload = multer(uploadConfig.upload("./tmp/cars"));
+const upload = multer(uploadConfig);
 
 carsRoutes.post(
   "/",
