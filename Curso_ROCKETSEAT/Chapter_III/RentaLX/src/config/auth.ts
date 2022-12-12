2d6613a84b8842c9064b076e5232eabb79c669fc
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2022-12-12 10:29:59 -0300

    Upload using S3 of Amazon

--- Curso_ROCKETSEAT/Chapter_III/RentaLX/src/config/auth.ts
+++ Curso_ROCKETSEAT/Chapter_III/RentaLX/src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   secret_token: "284fda2c-11f6-4853-b07c-f098d0f9b7ed",
-  expires_in_token: "30s",
+  expires_in_token: "50s",
   secret_refresh_token: "5b6cfe40-6a18-47d0-9e22-1335c41d224f",
   expires_in_refresh_token: "30d",
   expires_refresh_token_days: 30,
