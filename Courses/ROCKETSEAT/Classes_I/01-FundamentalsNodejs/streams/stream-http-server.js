d19e55064913675ac764e5cc3a6e24900b8102bb
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2023-01-13 11:17:35 -0300

    Consume one stream full

--- Courses/ROCKETSEAT/Classes_I/01-FundamentalsNodejs/streams/stream-http-server.js
+++ Courses/ROCKETSEAT/Classes_I/01-FundamentalsNodejs/streams/stream-http-server.js
@@ -7,10 +7,16 @@ class InverseNumberStream extends Transform {
   }
 }
 
-const server = http.createServer((request, response) => {
-  return request
-    .pipe(new InverseNumberStream())
-    .pipe(response);
+const server = http.createServer(async (request, response) => {
+  const buffers = [];
+
+  for await(const chunk of request) {
+    buffers.push(chunk);
+  }
+
+  const fullStreamContent = Buffer.concat(buffers).toString();
+
+  return response.end(fullStreamContent);
 });
 
 server.listen(3334);
