12d95c17cf7a09f047e5e9eedef300902681d3dd
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2023-01-12 21:58:45 -0300

    Create and List with methods HTTP

--- Courses/ROCKETSEAT/Classes_I/01-FundamentalsNodejs/src/server.js
+++ Courses/ROCKETSEAT/Classes_I/01-FundamentalsNodejs/src/server.js
@@ -1,7 +1,17 @@
 import http from "node:http";
 
 const server = http.createServer((request, response) => {
+  const { method, url } = request;
+
+  if( method === "GET" && url === "/users") {
+    return response.end("List of users");
+  }
+
+  if( method === "POST" && url === "/users") {
+    return response.end("Create of users");
+  }
+
   return response.end("Hello World");
 });
 
-server.listen(3456);
+server.listen(3333);
